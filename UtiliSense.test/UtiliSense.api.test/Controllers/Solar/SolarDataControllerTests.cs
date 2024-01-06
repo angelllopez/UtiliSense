ed6c24c9a976c642306d680833c2b7cc62d2315f
A. Lopez <140568825+angelllopez@users.noreply.github.com>
2024-01-06 17:44:14 -0600

    Added SolarDataControllerTest Added SolarDataController

---
+++ UtiliSense.test/UtiliSense.api.test/Controllers/Solar/SolarDataControllerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace UtiliSense.test.UtiliSense.api.test.Controllers.Solar
+{
+    public class SolarDataControllerTests
+    {
+        [Fact]
+        public async Task GetSolarDataAsync_ReturnsOk_WhenRecordsExist()
+        {
+            // Arrange
+            //var stubList = new List<SolarData> { new SolarData() };
+            //var mockService = new Mock<SolarDataService>(MockBehavior.Strict);
+
+            //mockService.Setup(s => s.GetAllSolarDataAsync()).ReturnsAsync(stubList);
+
+            //var controller = new SolarDataController(mockService.Object);
+
+            // Act
+            //var result = await controller.GetSolarDataAsync();
+
+            // Assert
+            //Assert.NotEmpty(result);
+            //Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
