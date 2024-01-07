aabd16986c88d3461c771b9cf836b3966a9542ae
A. Lopez <140568825+angelllopez@users.noreply.github.com>
2024-01-06 19:39:48 -0600

    Added Services.

---
+++ UtiliSense.service/SolarDataService.cs
@@ -0,0 +1,43 @@
+﻿using UtiliSense.data.Models;
+using UtiliSense.service.Contracts;
+
+namespace UtiliSense.service
+{
+    public class SolarDataService : ISolarDataService
+    {
+        public Task<bool> CreateSolarDataRecord(SolarData solarData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteSolarDataRecord(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SolarData>> GetAllSolarDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SolarData> GetSolarDataByDay(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SolarData>> GetSolarDataByMonth(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SolarData>> GetSolarDataByYear(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateSolarDataRecord(SolarData solarData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
